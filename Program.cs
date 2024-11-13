fb8fcf97770fd00520eaee07254243f5c85d30a5
solarisintel <solarisintel@gmail.com>
2024-11-13 23:49:48 +0900

    added http request

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using System.IO;
 
 // windowsイベント通知
 using Microsoft.Win32;
+using System.Net.Http;
 
 // 参考
 // https://qiita.com/tukiyo3/items/b11c796f1679cd605209
@@ -22,6 +23,8 @@ class MainWindow
 
 class Launcher : Form
 {
+    static String serverResponseJson;
+
     public Launcher()
     {
         this.ShowInTaskbar = false;
@@ -35,8 +38,38 @@ class Launcher : Form
 
     private void cmdIe()
     {
-        ProcessStartInfo processStartInfo = new ProcessStartInfo("C:\\Program Files (x86)\\Internet Explorer\\iexplore.exe", "http://yahoo.co.jp");
-        Process process = Process.Start(processStartInfo);
+        //ProcessStartInfo processStartInfo = new ProcessStartInfo("C:\\Program Files (x86)\\Internet Explorer\\iexplore.exe", "http://yahoo.co.jp");
+        //Process process = Process.Start(processStartInfo);
+
+        var jsonString = "{ \"age\" : 20, \"name\" : \"太郎\"  }";
+        Post("https://pochi.mydns.jp/json", jsonString);
+
+    }
+
+    private static async void Post(string url, string request)
+    {
+        using (var client = new HttpClient())
+        {
+            try
+            {
+                // タイムアウト時間の設定(3秒)
+                client.Timeout = TimeSpan.FromMilliseconds(3000);
+
+                //POSTリクエスト(JSON)
+                var content = new StringContent(request, System.Text.Encoding.UTF8, "application/json");
+                var res = await client.PostAsync(url, content);
+                //取得
+                serverResponseJson = await res.Content.ReadAsStringAsync();
+                Console.WriteLine(serverResponseJson);
+            }
+            catch (Exception e)
+            {
+                serverResponseJson = "error";
+                Console.WriteLine(e.Message);
+                MessageBox.Show("サーバから応答がありません");
+
+            }
+        }
     }
 
     private void Close_Click()
